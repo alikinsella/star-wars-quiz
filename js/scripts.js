622f19a6ab7cae37e3bf03e3a52f53c45927b378
Ali Kinsella <ali.kinsella@gmail.com>
2020-04-30 23:01:12 -0500

    introduce the celebrity quiz using html, css, js, and some heavy front-end logic. Good luck

---
+++ js/scripts.js
@@ -0,0 +1,175 @@
+// back-end logic
+
+var a = parsInt().val();
+var b = parsInt().val();
+var c = parsInt().val();
+var d = parsInt().val();
+// var flavor = $("input:radio[name=flavor]:checked").val();
+
+// front-end logic
+$(document).ready(function() {
+  $("form#").submit(function(event) {
+if (a >= 5) {
+  ("Chewbacca").show;
+} else if (b >= 5) {
+  ("Leia").show;
+} else if (c >= 5) {
+  ("Hans").show;
+} else if (d >= 5) {
+  ("Luke").show;
+} else if (a >= 4) {
+  ("Chewbacca").show;
+} else if (b >= 4) {
+  ("Leia").show;
+} else if (c >= 4) {
+  ("Hans").show;
+} else if (d >= 4) {
+  ("Luke").show;
+} else if (a >= 3) {
+  ("Chewbacca").show;
+} else if (b >= 3) {
+  ("Leia").show;
+} else if (c >= 3) {
+  ("Hans").show;
+} else (d >= 3) {
+  ("Luke").show;
+}
+
+// if (question1 && question2 && question3 === "a") {
+//   ("Chewbacca").show;
+// } else if (question1 && question2 && question4 === "a") {
+//   ("Chewbacca").show;
+// } else if (question1 && question2 && question5 === "a") {
+//   ("Chewbacca").show;
+// } else if (question2 && question3 && question4 === "a") {
+//   ("Chewbacca").show;
+// } else if (question2 && question3 && question5 === "a") {
+//   ("Chewbacca").show;
+// } else if (question3 && question4 && question5 === "a") {
+//   ("Chewbacca").show;
+// 
+
+// } else if (question1 && question2 && question3 === "b") {
+//   ("Leia").show;
+// } else if (question1 && question2 && question4 === "b") {
+//   ("Leia").show;
+// } else if (question1 && question2 && question5 === "b") {
+//   ("Leia").show;
+// } else if (question2 && question3 && question4 === "b") {
+//   ("Leia").show;
+// } else if (question2 && question3 && question5 === "b") {
+//   ("Leia").show;
+// } else if (question3 && question4 && question5 === "b") {
+//   ("Leia").show;
+// 
+// } else if (question1 && question2 && question3 === "c") {
+//   ("Hans").show;
+// } else if (question1 && question2 && question4 === "c") {
+//   ("Hans").show;
+// } else if (question1 && question2 && question5 === "c") {
+//   ("Hans").show;
+// } else if (question2 && question3 && question4 === "c") {
+//   ("Hans").show;
+// } else if (question2 && question3 && question5 === "c") {
+//   ("Hans").show;
+// } else if (question3 && question4 && question5 === "c") {
+//   ("Hans").show;
+// 
+// } else if (question1 && question2 && question3 === "b") {
+//   ("Luke").show;
+// } else if (question1 && question2 && question4 === "b") {
+//   ("Luke").show;
+// } else if (question1 && question2 && question5 === "d") {
+//   ("Luke").show;
+// } else if (question2 && question3 && question4 === "d") {
+//   ("Luke").show;
+// } else if (question2 && question3 && question5 === "d") {
+//   ("Luke").show;
+// } else if (question3 && question4 && question5 === "d") {
+//   ("Luke").show;
+// 
+
+// } else if (question1 && question2 || question3 || question4 || question5 === "a") {
+//   ("Chewbacca").show;;
+// } else if (question2 && question3 || question4 || question5 === "a") {
+//   ("Chewbacca").show;
+// } else if (question3 && question4 || question5 === "a") {
+//   ("Chewbacca").show;
+// } else if (question4 && question5 === "a") {
+//   ("Chewbacca").show;
+
+
+// } else if (question1 && question2 || question3 || question4 || question5 === "b") {
+//   ("Leia").show;
+// } else if (question2 && question3 || question4 || question5 === "b") {
+//   ("Leia").show;
+// } else if (question3 && question4 || question5 === "b") {
+//   ("Leia").show;
+// } else if (question4 && question5 === "b") {
+//   ("Leia").show;
+
+
+// } else if (question1 && question2 || question3 || question4 || question5 === "c") {
+//   ("Hans").show;
+// } else if (question2 && question3 || question4 || question5 === "c") {
+//   ("Hans").show;
+// } else if (question3 && question4 || question5 === "c") {
+//   ("Hans").show;
+// } else if (question4 && question5 === "c") {
+//   ("Hans").show;
+
+
+// } else if (question1 && question2 || question3 || question4 || question5 === "d") {
+//   ("Luke").show;
+// } else if (question2 && question3 || question4 || question5 === "d") {
+//   ("Luke").show;
+// } else if (question3 && question4 || question5 === "d") {
+//   ("Luke").show;
+// } else if (question4 && question5 === "d") {
+//   ("Luke").show;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} else if (a && c) {
+//   ("Hans").show;
+// } else if (a && d) {
+//   ("Luke").show;
+// } else if (b && a) {
+//   ("Chewbacca").show;
+// } else if (b && b) {
+//   ("Leia").show;
+// } else if (b && c) {
+//   ("Hans").show;
+// } else if (b && d) {
+//   ("Luke").show;
+// } else if (c && a) {
+//   ("Chewbacca").show;
+// } else if (c && b) {
+//   ("Leia").show;
+// } else if (c && c) {
+//   ("Hans").show;
+// } else (c && d) {
+//   ("Luke").show;
+// }} else if (d && a) {
+//   ("Chewbacca").show;
+// } else if (d && b) {
+//   ("Leia").show;
+// } else if (d && c) {
+//   ("Hans").show;
+// } else (d && d) {
+//   ("Luke").show;
+// }
+  
+// }
